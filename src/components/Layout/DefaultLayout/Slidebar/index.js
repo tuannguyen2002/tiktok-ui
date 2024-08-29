9e2592ab1c68e321aff23b014f4f4069d4e41487
tuannguyen <coixaygio107@gmail.com>
2024-08-29 21:25:11 +0700

    Build default layout frame

--- src/components/Layout/DefaultLayout/Slidebar/index.js
+++ src/components/Layout/DefaultLayout/Slidebar/index.js
@@ -1,5 +1,12 @@
+import styles from './Slidebar.module.scss'
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles)
+
 function Slidebar() {
-    return <h2>Slidebar</h2>;
+    return <aside className={cx('wrapper')}>
+        <h2>Side bar</h2>
+    </aside>
 }
 
 export default Slidebar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss'
 import Header from '~/components/Layout/components/Header'
 import Slidebar from './Slidebar'
 
+const cx = classNames.bind(styles)
+
 function DefaultLayout({ children }) {
     return ( 
-        <div>
+        <div className={cx('wrapper')}> 
             <Header />
-            <div className='container'>
+            <div className={cx('container')}>
                 <Slidebar />
-                <div className='content'>
+                <div className={cx('content')}>
                     {children}
                 </div>
             </div>
