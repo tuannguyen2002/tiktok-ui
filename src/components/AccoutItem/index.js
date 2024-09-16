5cc090aff94835732e1af7fdc54a9d316da8d7ec
tuannguyen <coixaygio107@gmail.com>
2024-09-16 21:43:58 +0700

    Build logic for search box #2

--- src/components/AccoutItem/index.js
+++ src/components/AccoutItem/index.js
@@ -1,27 +1,28 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import Image from '~/components/Image'
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
 
-function AccoutItem() {
+function AccoutItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
             <Image
-                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/b2b0c654a9266e8b084cd824c2c4ef4e.jpeg?lk3s=a5d48078&nonce=9777&refresh_token=c5bb0b704f3ad88179b498fa4dd440b4&x-expires=1725523200&x-signature=llQH3w0HeMe6DC6BhC51f66HrfY%3D&shp=a5d48078&shcp=81f88b70"
-                alt="Hoaa"
+                src={data.avatar}
+                alt={data.full_name}
                 className={cx('avatar')}
             />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen Minh Hoa</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>Hoaa.nguyen</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,11 +10,11 @@ import Search from '~/pages/Search';
 
 //public routes
 const publicRoutes = [
-    { path: '/', component: Home},
-    { path: '/following', component: Following},
-    { path: '/profile', component: Profile},
-    { path: '/upload', component: Upload, layout: HeaderOnly},
-    { path: '/search', component: Search, layout: null},
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/@:nickname', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
 ];
 
 
